0b418bff9858daffd5dfece86f847efe0fdf2154
luosilent <luosilent@qq.com>
2018-05-04 03:23:14 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+             by <a href="https://github.com/luosilent" target="_blank">luosilent Github </a> <span style="color: #e27575;font-size: 14px;">star❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:luosilent@163.com">联系我</a></p>
+    </div>
+</footer>
