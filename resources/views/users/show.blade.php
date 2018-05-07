102337088bf4999d49768b327bde2beab21cc613
luosilent <luosilent@qq.com>
2018-05-07 09:45:18 +0000

    Basic user profile editing

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,13 +13,13 @@
                     <div align="center">
                         <img class="thumbnail img-responsive" src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" width="300px" height="300px">
                     </div>
-                    <div class="media-body">
+                     <div class="media-body">
                         <hr>
                         <h4><strong>个人简介</strong></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                        <p>{{ $user->introduction }}</p>
                         <hr>
                         <h4><strong>注册于</strong></h4>
-                        <p>January 01 1901</p>
+                        <p>{{ $user->created_at->diffForHumans() }}</p>
                     </div>
                 </div>
             </div>
