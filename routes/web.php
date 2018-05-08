1db426f0fa5467b5994002c2250834079a1d1105
luosilent <luosilent@qq.com>
2018-05-08 01:43:18 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
