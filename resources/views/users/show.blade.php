0dcd1de222bc4c66956f0e1ed73a6a652b5c4ed0
luosilent <luosilent@qq.com>
2018-05-08 03:24:34 +0000

    List topics created by user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -38,10 +38,13 @@
         {{-- 用户发布的内容 --}}
         <div class="panel panel-default">
             <div class="panel-body">
-                暂无数据 ~_~
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#">Ta 的话题</a></li>
+                    <li><a href="#">Ta 的回复</a></li>
+                </ul>
+                @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
             </div>
         </div>
-
     </div>
 </div>
 @stop
