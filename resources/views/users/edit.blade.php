e2096a6e439538a10f6528322c30e66e751dcede
luosilent <luosilent@qq.com>
2018-05-08 01:12:06 +0000

    User can upload avatar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,10 +11,10 @@
         </div>
 
         @include('common.error')
-        
+
         <div class="panel-body">
 
-            <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+            <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8"  enctype="multipart/form-data">
                 <input type="hidden" name="_method" value="PUT">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -26,9 +26,19 @@
                     <label for="email-field">邮 箱</label>
                     <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email ) }}" />
                 </div>
-                <div class="form-group">
+                 <div class="form-group">
                     <label for="introduction-field">个人简介</label>
                     <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction ) }}</textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="" class="avatar-label">用户头像</label>
+                    <input type="file" name="avatar">
+
+                    @if($user->avatar)
+                        <br>
+                        <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="200" />
+                    @endif
                 </div>
                 <div class="well well-sm">
                     <button type="submit" class="btn btn-primary">保存</button>
