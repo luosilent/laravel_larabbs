f87c2a2c6c3112460c0f76b4baf9a573d3d3ec8a
luosilent <luosilent@qq.com>
2018-05-09 03:37:23 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -9,8 +9,10 @@ use App\Models\Topic;
 
 class TopicObserver
 {
-      public function saving(Topic $topic)
+    public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
