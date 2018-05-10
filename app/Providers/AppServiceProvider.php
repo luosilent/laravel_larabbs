93c30ba4947ea910154528b421741ec217c7d6e0
luosilent <luosilent@qq.com>
2018-05-10 09:06:45 +0000

    Sidebar links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,11 +12,13 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-	{
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+    {
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
+        // Carbon 中文化配置
         \Carbon\Carbon::setLocale('zh');
     }
 
