6a4c9342f0a0a67d0b91bd4f5cc5acd444399697
luosilent <luosilent@qq.com>
2018-05-10 07:58:03 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
              by <a href="https://github.com/luosilent" target="_blank">luosilent Github </a> <span style="color: #e27575;font-size: 14px;">star❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:luosilent@163.com">联系我</a></p>
+       <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
