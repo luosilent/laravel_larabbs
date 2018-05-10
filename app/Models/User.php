a4eef69cae77c8cbef8da6f0e94721883bb714c3
luosilent <luosilent@qq.com>
2018-05-10 08:43:50 +0000

    Sidebar active users

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasRoles;
-
+    use Traits\ActiveUserHelper;
     use Notifiable {
         notify as protected laravelNotify;
     }
